97f7d3ce690536ab5ee90ddc5b40f2f534dc964f
DIPANKER-PC\Dipanker <dipankarai@gmail.com>
2017-02-17 09:27:44 +0530

    Wikitude Plugins.

---
+++ Assets/Wikitude/Samples/Scripts/InputPlugin/CustomCameraController.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System;
+using Wikitude;
+using System.Runtime.InteropServices;
+
+/// <summary>
+/// Handles getting the camera frame from the device and forwarding it to the Wikitude SDK.
+/// </summary>
+public class CustomCameraController : SampleController
+{
+	public WikitudeCamera WikitudeCam;
+
+	protected WebCamTexture _feed;
+	protected bool _imageUpsideDown = false;
+
+	public const int FrameWidth = 640;
+	public const int FrameHeight = 480;
+
+	private int _frameDataSize = 0; 
+	private int _frameIndex = 0;
+	private Color32[] _pixels;
+
+	public void OnInputPluginRegistered() {
+		Initialize();
+	}
+
+	protected virtual void Initialize() {
+		foreach (var device in WebCamTexture.devices) {
+			if (!device.isFrontFacing) {
+				_feed = new WebCamTexture(device.name, FrameWidth, FrameHeight);
+				_feed.Play();
+				break;
+			}
+		}
+
+		if (_feed == null) {
+			Debug.LogError("Could not find any cameras on the device.");
+		}
+
+		ResetBuffers(FrameWidth, FrameHeight, 4);
+
+		if (_feed.videoRotationAngle == 270) {
+			// If the camera angle is 270, it means that it is inverted, such as the camera found on the 5X, 
+			// so we flip the invert and mirror flags on the camera to rotate the image upside down
+			WikitudeCam.InvertedFrame = false;
+			WikitudeCam.EnableMirroring = true;
+
+			_imageUpsideDown = true;
+		}
+	}
+
+	protected virtual void ResetBuffers(int width, int height, int bytesPerPixel) {
+		_frameDataSize = width * height * bytesPerPixel;
+		_pixels = new Color32[width * height];
+
+		WikitudeCam.InputFrameWidth = width;
+		WikitudeCam.InputFrameHeight = height;
+	}
+
+	protected override void Update() {
+		base.Update();
+		if (_feed == null || !_feed.didUpdateThisFrame) {
+			return;
+		}
+
+		if (_feed.width != FrameWidth || _feed.height != FrameHeight) {
+			Debug.LogError("Camera feed has unexpected size.");
+			return;
+		}
+
+		int newFrameDataSize = _feed.width * _feed.height * 4;
+		if (newFrameDataSize != _frameDataSize) {
+			ResetBuffers(_feed.width, _feed.height, 4);
+		}
+
+		_feed.GetPixels32(_pixels);
+		SendNewCameraFrame();
+	}
+
+	private void SendNewCameraFrame() {
+		GCHandle handle = default(GCHandle);
+		try {
+			handle = GCHandle.Alloc(_pixels, GCHandleType.Pinned);
+			IntPtr frameData = handle.AddrOfPinnedObject();
+			WikitudeCam.NewCameraFrame(++_frameIndex, _frameDataSize, frameData);
+		} finally {
+			if (handle != default(GCHandle)) {
+				handle.Free();
+			}
+		}
+	}
+
+	protected virtual void Cleanup() {
+		_frameDataSize = 0;
+		if (_feed != null) {
+			_feed.Stop();
+			_feed = null;
+		}
+	}
+
+	private void OnApplicationPause(bool paused) {
+		if (paused) {
+			Cleanup();
+		} else {
+			Initialize();
+		}
+	}
+
+	private void OnDestroy() {
+		Cleanup();
+	}
+}
---
+++ Assets/Wikitude/Samples/Scripts/InputPlugin/CustomCameraRenderer.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+/// <summary>
+/// Handles custom camera rendering of a frame.
+/// </summary>
+public class CustomCameraRenderer : MonoBehaviour 
+{
+	public Material EffectMaterial;
+
+	private Texture _currentFrame;
+	public Texture CurrentFrame {
+		set {
+			_currentFrame = value;
+			enabled = true;
+			SetCommandBuffer();
+		}
+	}
+
+	private struct ImageRotation {
+		public bool FlipHorizontally;
+		public bool FlipVertically;
+		public bool Rotate;
+
+		public ImageRotation(bool flipHorizontally, bool flipVertically, bool rotate) {
+			FlipHorizontally = flipHorizontally;
+			FlipVertically = flipVertically;
+			Rotate = rotate;
+		}
+	}
+
+	[HideInInspector]
+	public bool FlipImage = false;
+
+	private CommandBuffer _drawFrameBuffer;
+	private int _currentScreenWidth = 0;
+
+	private void SetCommandBuffer() {
+
+		var camera = GetComponent<Camera>();
+		CameraEvent eventForBlit;
+
+		if (camera.actualRenderingPath == RenderingPath.Forward) {
+			eventForBlit = CameraEvent.BeforeForwardOpaque;
+		} else {
+			eventForBlit = CameraEvent.BeforeGBuffer;
+		}
+
+		if (_drawFrameBuffer != null) {
+			camera.RemoveCommandBuffer(eventForBlit, _drawFrameBuffer);
+		}
+
+		if (_currentFrame != null) {
+			EffectMaterial.SetInt("_ResolutionX", CustomCameraController.FrameWidth);
+			EffectMaterial.SetInt("_ResolutionY", CustomCameraController.FrameHeight);
+
+			_drawFrameBuffer = new CommandBuffer();
+			_drawFrameBuffer.Blit(_currentFrame, BuiltinRenderTextureType.CameraTarget, EffectMaterial);
+			camera.AddCommandBuffer(eventForBlit, _drawFrameBuffer);
+		}
+	}
+
+	void Update() {
+		if (_currentFrame.width == CustomCameraController.FrameWidth && _currentFrame.height == CustomCameraController.FrameHeight) {
+			if (Screen.width != _currentScreenWidth && Screen.orientation != ScreenOrientation.Unknown) {
+				_currentScreenWidth = Screen.width;
+				UpdateOrientation(Screen.orientation);
+			}
+		}
+	}
+
+	public void UpdateOrientation(ScreenOrientation screenOrientation) {
+		var rotation = new ImageRotation(false, false, false);
+		
+		switch (screenOrientation) {
+			case ScreenOrientation.LandscapeLeft:
+				rotation = new ImageRotation(false, false, false);
+				break;
+			case ScreenOrientation.LandscapeRight:
+				rotation = new ImageRotation(true, true, false);
+				break;
+			case ScreenOrientation.Portrait:
+				rotation = new ImageRotation(false, true, true);
+				break;
+			case ScreenOrientation.PortraitUpsideDown:
+				rotation = new ImageRotation(true, false, true);
+				break;
+		}
+
+		if (FlipImage) {
+			rotation.FlipVertically = !rotation.FlipVertically;
+			rotation.FlipHorizontally = !rotation.FlipHorizontally;
+		}
+
+		SetImageRotation(rotation);
+
+		float frameAspectRatio = (float)_currentFrame.width / (float)_currentFrame.height;
+		float screenAspectRatio = (float)Screen.width / (float)Screen.height;
+
+		float ratio = 1.0f;
+
+		switch (screenOrientation) {
+		case ScreenOrientation.LandscapeLeft:
+		case ScreenOrientation.LandscapeRight:
+			ratio = frameAspectRatio / screenAspectRatio;
+			break;
+		case ScreenOrientation.Portrait:
+		case ScreenOrientation.PortraitUpsideDown:
+			ratio = frameAspectRatio * screenAspectRatio;
+			break;
+		}
+
+		EffectMaterial.SetFloat("_Scale", ratio);
+		EffectMaterial.SetFloat("_Pan", (1.0f - ratio) / 2.0f);
+	}
+
+	void SetImageRotation(ImageRotation rotation) {
+		EffectMaterial.SetFloat("_FlipU", rotation.FlipHorizontally ? 1 : 0);
+		EffectMaterial.SetFloat("_FlipV", rotation.FlipVertically ? 1 : 0);
+		EffectMaterial.SetFloat("_Rotate", rotation.Rotate ? 1 : 0);
+	}
+}
